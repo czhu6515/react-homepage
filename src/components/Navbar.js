48f510e56abfa8cad5f993a13605dd54fdd4d76c
Chen <chenyu.zhu86@gmail.com>
2018-11-22 18:25:12 -0500

    changed buttons for nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,26 +2,13 @@ import React from "react";
 import Button from "@material-ui/core/Button";
 
 class Navbar extends React.Component {
-
   render() {
     return (
       <nav className="nav-bar">
         <ul>
-          <li>
-            <Button variant="outlined" onClick={this.props.renderHome}>
-              Home
-            </Button>
-          </li>
-          <li>
-            <Button variant="outlined" onClick={this.props.renderProjects}>
-              Projects
-            </Button>
-          </li>
-          <li>
-            <Button variant="outlined" onClick={this.props.renderContacts}>
-              Contacts
-            </Button>
-          </li>
+          <li onClick={this.props.renderHome}>Home</li>
+          <li onClick={this.props.renderProjects}>Projects</li>
+          <li onClick={this.props.renderContacts}> Contacts</li>
         </ul>
       </nav>
     );
@@ -29,13 +16,3 @@ class Navbar extends React.Component {
 }
 
 export default Navbar;
-
-{
-  /* <Home />
-        <Main />
-        <Projects />
-        <Resume />
-        <Contact /> 
-        <SocialLinks />
-        <Footer /> */
-}
