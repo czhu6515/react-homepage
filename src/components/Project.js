c1a67b482ae8136ddfdd3ff5d0b20dbce48cbaf0
Chen <chenyu.zhu86@gmail.com>
2018-11-27 18:07:59 -0500

    working on arrows for react project component

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,17 +4,25 @@ import mockup from '../imgs/mock-up.png'
 class Project extends Component {
   render() {
     return(
-      <div id="project-wrapper">
-        <h2 id="project-title">Hello!</h2>
-        <div id="project-links-wrapper">
-          <a id="project-url" href="https://czhu6515.github.io/projects/week1project-master/index.html">
-            <i class="fa-3x fas fa-link"></i>
-          </a>
-          <a id="project-github" href="https://czhu6515.github.io/projects/week1project-master/index.html">
-            <i class="fa-3x fab fa-github"></i>
-          </a>
+      <div id="project-main-wrapper">
+        <div id="project-carousel">
+          <i class="fa-3x fas fa-angle-left"></i>
+          <i class="fa-3x fas fa-angle-right"></i>
         </div>
+        <div id="project-wrapper">
+        
           <img id="project-img" src={mockup} alt="Mock-Up" />
+
+          <div id="project-links-wrapper" class="animated fadeInDown">
+            <a id="project-url" href="https://czhu6515.github.io/projects/week1project-master/index.html">
+              <i className="fa-3x fas fa-link"></i>
+            </a>
+            <a id="project-github" href="https://czhu6515.github.io/projects/week1project-master/index.html">
+              <i className="fa-3x fab fa-github"></i>
+            </a>
+            
+          </div>
+        </div>
       </div>
     )
   }
