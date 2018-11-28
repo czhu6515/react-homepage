37bfb52dc5509b2d452824f6a9e2793ba616cf3b
Chen <chenyu.zhu86@gmail.com>
2018-11-28 11:54:27 -0500

    added drop shadow to carousel

--- src/components/Project.js
+++ src/components/Project.js
@@ -6,10 +6,20 @@ class Project extends Component {
       <div id="project-wrapper" className="animated fadeIn">
         <img id="project-img" src={this.props.img} alt={this.props.alt} />
         <div id="project-links-wrapper" className="animated fadeInDown">
-          <a id="project-url" href={this.props.url}>
+          <a
+            id="project-url"
+            href={this.props.url}
+            rel="noopener noreferrer"
+            target="_blank"
+          >
             <i className="fa-3x fas fa-link" />
           </a>
-          <a id="project-github" href={this.props.git}>
+          <a
+            id="project-github"
+            href={this.props.git}
+            rel="noopener noreferrer"
+            target="_blank"
+          >
             <i className="fa-3x fab fa-github" />
           </a>
         </div>
